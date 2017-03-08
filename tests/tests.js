a3b7a23d22f065996c7c7f2d0d9d17d708bcee61
finnhodgkin <finnhodgkin@gmail.com>
2017-03-08 15:23:00 +0000

    add first test for the giphy function to check it outputs and array of the correct length relates to #29

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,9 @@
 module('createLapElement');
-test('createLapElement makes a human readable lap element', (assert) => {
-  var result = createLapElement(0, 500, 'testing').innerText;
-  var expected = 'Lap number 1 00:00.50';
-  assert.equal(result, expected);
+test('giffy function', (assert) => {
+  var done = assert.async();
+  var obj = {name: "Lord of the Rings"};
+  giphy(obj, function(err, res) {
+    assert.ok(res.length === 4);
+    done();
+  });
 });
