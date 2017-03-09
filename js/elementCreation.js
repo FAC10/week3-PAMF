77d20042a59823560aa0ed723bd795a41fd6e8df
pbywater <philippabywater@gmail.com>
2017-03-09 13:16:33 +0000

    appending words and columns working
    relates to #32

--- js/elementCreation.js
+++ js/elementCreation.js
@@ -26,11 +26,57 @@ function create (element, cls, txt, src) {
   }
   return el;
 }
-  return { create:create, howManyGifs:howManyGifs };
+
+function appendClass(parent, child, i) {
+  var parents = [].slice.call(document.getElementsByClassName(parent));
+  console.log(parents[0]);
+  parents[i].appendChild(child);
+  console.log('parents ' + i + ' is ' + parents[i]);
+}
+
+function appendQuery(parent, child) {
+  document.querySelector(parent).appendChild(child);
+}
+// var water = document.createTextNode("Water");
+// append('.flex', water);
+
+function columnAppend(num) {
+while (num > 0) {
+var column = create('section', 'columns', '', '');
+appendQuery('.flex', column);
+num--;
+}
+}
+
+function wordAppend() {
+  console.log('trying to word append');
+var num = document.getElementsByClassName('columns').length;
+for (var i = 0; i < num; i++) {
+  console.log('entering for loop');
+  var word = create('p', '', 'word ' + (i+1), '');
+  console.log(word);
+  appendClass('columns', word, i);
+}
+}
+
+function gifCreation(cb) {
+  var img = create('img', '', '', 'https://www.hello.com/img_/hello_logo_hero.png')
+  return cb(img);
+}
+
+function gifAppend() {
+  var num = document.getElementsByClassName('columns').length;
+for (var i = 0; i < num; i++) {
+  appendClass('columns', gifCreation, i);
+  // append(array[i], gifCreation);
+  // append(array[i], gifCreation);
+}
+}
+
+  return { create:create, howManyGifs:howManyGifs, columnAppend:columnAppend, wordAppend:wordAppend, gifAppend:gifAppend};
 })();
+var arr = [];
 
-//tests
-
-createElements.create('p', 'columns',null, 'https://www.hello.com/img_/hello_logo_hero.png');
-// //
-// howManyGifs(['www.facebook.com', 'OF', "google.com", 'THE']);
+createElements.columnAppend(4);
+createElements.wordAppend();
+createElements.gifAppend();
