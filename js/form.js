e463aef6c297fe01eec9cf34fcb78001bc162c44
finnhodgkin <finnhodgkin@gmail.com>
2017-03-09 18:15:48 +0000

    move output result div relates to #59

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
-// Attaches the Form
 var attachForm = (function (){
-// Displays the form and attaches eventListener to submit button
+
   var addFormEvent = function (obj) {
     var form = document.getElementsByTagName('form')[0];
     form.style.display = 'inline';
@@ -9,15 +8,14 @@ var attachForm = (function (){
       answerValidation(e.target[0].value, obj.name);
     });
   };
-  // Validates user answer
   var answerValidation = function (user_answer, answer){
     user = user_answer.split(' ');
     ans = answer.split(' ');
     if (answer.toLowerCase() === user_answer.toLowerCase() ) {
-      document.getElementById('id').innerText = 'Correct! The film was ' + answer;
+      document.getElementById('output').innerText = 'Correct! The film was ' + answer;
     }
     else {
-      document.getElementById('id').innerText = 'Correct! The film was ' + answer;
+      document.getElementById('output').innerText = 'Correct! The film was ' + answer;
     }
   };
 
